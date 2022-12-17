93fb745f92de458ddad830e6b95b91027e514dc2
nashdashin <srivastavanikhil226@yahoo.ca>
2022-12-17 11:40:23 -0700

    dosage changes

--- TransferMobileAPP/TransferMobileAPP/Form1.cs
+++ TransferMobileAPP/TransferMobileAPP/Form1.cs
@@ -19,8 +19,131 @@ namespace TransferMobileAPP
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int Pounds = int.Parse(kgBox.Text);
-            actualCalculation.Text = Pounds.ToString();
+            double kilograms = double.Parse(kgBox.Text);
+
+            if (kilograms >= 20 / 2.2 && kilograms < 23 / 2.2) {
+
+                actualCalculation.Text = "test";
+            }
+
+            else if (kilograms >= 23 / 2.2 && kilograms < 24 / 2.2)
+            {
+                actualCalculation.Text = "test2";
+
+            }
+
+            else if (kilograms >= 30 / 2.2 && kilograms < 31 / 2.2)
+            {
+
+           
+
+            }
+
+            else if (kilograms >= 31 / 2.2 && kilograms < 36 / 2.2)
+            {
+
+            
+
+            }
+
+            else if (kilograms >= 36 / 2.2 && kilograms < 38 / 2.2)
+            {
+
+            
+
+            }
+
+            else if (kilograms >= 38 / 2.2 && kilograms < 42 / 2.2)
+            {
+
+             
+
+            }
+
+            else if (kilograms >= 42 / 2.2 && kilograms < 46 / 2.2)
+            {
+
+          
+            }
+
+            else if (kilograms >= 46 / 2.2 && kilograms < 48 / 2.2)
+            {
+
+         
+
+            }
+
+
+
+            else if (kilograms >= 48 / 2.2 && kilograms < 51 / 2.2)
+            {
+
+         
+
+            }
+
+            else if (kilograms >= 51 / 2.2 && kilograms < 56 / 2.2)
+            {
+
+             
+
+            }
+
+            else if (kilograms >= 56 / 2.2 && kilograms < 60 / 2.2)
+            {
+
+     
+
+            }
+
+            else if (kilograms >= 60 / 2.2 && kilograms < 62 / 2.2)
+            {
+
+  
+
+            }
+
+
+            else if (kilograms >= 62 / 2.2 && kilograms < 65 / 2.2)
+            {
+
+       
+
+            }
+
+            else if (kilograms >= 65 / 2.2 && kilograms < 71 / 2.2)
+            {
+
+
+            }
+
+            else if (kilograms >= 71 / 2.2 && kilograms < 78 / 2.2)
+            {
+
+         
+            }
+
+            else if (kilograms >= 78 / 2.2 && kilograms < 79 / 2.2)
+            {
+
+          
+
+            }
+
+
+            else if (kilograms >= 79 / 2.2 && kilograms < 86 / 2.2)
+            {
+
+          
+
+            }
+
+
+            else if (kilograms >= 86 / 2.2)
+            {
+
+
+            }
 
         }
 
